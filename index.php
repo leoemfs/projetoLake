7832ef5a9c963db4b5a43b855e13326e533f53f4
leoemfs <116128056+leoemfs@users.noreply.github.com>
2022-11-01 13:20:45 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv=" X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-widht, initial-scale=1.0">
+    <title>Home-Lake Street</title>
+</head>
+<body>
+    <div>
+        <h3>Home</h3>
+    </div>
+    <div>
+        <h4>
+          <a href="identificacao.php" target="_self">
+            Conta - Fazer login ou cadastro
+          </a>
+        </h4>
+    </div>
+</body>
+</html>
